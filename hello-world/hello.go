ff790afe1c0f2786b5ffedfcb646f9394a7e29d3
João Lourenço <joaodslourenco@gmail.com>
2024-01-23 10:39:09 -0300

    feat: add spanish greeting

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == spanish {
+		return fmt.Sprintf("%v%v!", spanishHelloPrefix, name)
+	}
+
 	return fmt.Sprintf("%v%v!", englishHelloPrefix, name)
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
