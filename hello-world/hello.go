700305a95208ab06acc47e4722d5e5c895cec7f8
João Lourenço <joaodslourenco@gmail.com>
2024-01-23 11:34:42 -0300

    fix: modify package name

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,4 +1,4 @@
-package main
+package hello
 
 import "fmt"
 
@@ -33,8 +33,4 @@ func Hello(name, language string) string {
 	}
 
 	return fmt.Sprintf("%v%v!", greetingPrefix(language), name)
-}
-
-func main() {
-	fmt.Println(Hello("Chris", ""))
 }
