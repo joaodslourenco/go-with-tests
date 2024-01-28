40462d57aca4b075632bcde0751882f793c7a012
João Lourenço <joaodslourenco@gmail.com>
2024-01-28 16:35:07 -0300

    feat: add countdown test and func

---
+++ mocking/mocking.go
@@ -0,0 +1,28 @@
+package mocking
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out *bytes.Buffer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
