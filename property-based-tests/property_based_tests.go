a3f31fefab5d8632003cf61eef3a0963eb525be2
Joao <joaodslourenco@gmail.com>
2024-02-24 17:56:31 -0300

    feat: add convertToRoman func and tests

---
+++ property-based-tests/property_based_tests.go
@@ -0,0 +1,37 @@
+package propertybasedtests
+
+import "strings"
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{Value: 1000, Symbol: "M"},
+	{Value: 900, Symbol: "CM"},
+	{Value: 500, Symbol: "D"},
+	{Value: 400, Symbol: "CD"},
+	{Value: 100, Symbol: "C"},
+	{Value: 90, Symbol: "XC"},
+	{Value: 50, Symbol: "L"},
+	{Value: 40, Symbol: "XL"},
+	{Value: 10, Symbol: "X"},
+	{Value: 9, Symbol: "IX"},
+	{Value: 5, Symbol: "V"},
+	{Value: 4, Symbol: "IV"},
+	{Value: 1, Symbol: "I"},
+}
+
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
