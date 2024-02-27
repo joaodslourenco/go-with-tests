844870ce0a82455348bb9bdd2adf51e8b4dedc88
Joao <joaodslourenco@gmail.com>
2024-02-27 09:15:17 -0300

    feat: add func and tests with generics

---
+++ arrays-and-slices-with-generics/arrays_and_slices_test.go
@@ -0,0 +1,125 @@
+package arraysandslices
+
+import (
+	"reflect"
+	"testing"
+)
+
+func assertCorrectMessage(t testing.TB, got int, want int, array []int) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, array)
+	}
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+
+	if got {
+		t.Errorf("got %v, want false", got)
+	}
+
+}
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
+
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+
+	if got == want {
+		t.Errorf("didn't want %+v", got)
+	}
+}
+
+func TestSum(t *testing.T) {
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		assertCorrectMessage(t, got, want, numbers)
+
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func TestBadBank(t *testing.T) {
+	transactions := []Transaction{
+		{
+			From: "Chris",
+			To:   "Riya",
+			Sum:  100,
+		},
+		{
+			From: "Adil",
+			To:   "Chris",
+			Sum:  25,
+		},
+	}
+
+	AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
+	AssertEqual(t, BalanceFor(transactions, "Chris"), -75)
+	AssertEqual(t, BalanceFor(transactions, "Adil"), -25)
+
+}
